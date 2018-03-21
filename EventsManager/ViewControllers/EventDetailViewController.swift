96557a3e86906f1e290013cee41c81ab13c73a53
wsjnohyeah <ethan.hqc@icloud.com>
2018-03-21 16:18:43 -0400

    fixed button style & added constraint constants in details

--- EventsManager/ViewControllers/EventDetailViewController.swift
+++ EventsManager/ViewControllers/EventDetailViewController.swift
@@ -11,6 +11,18 @@ import Kingfisher
 
 class EventDetailViewController: UIViewController {
     
+    //Constants
+    let buttonHeight = CGFloat(integerLiteral: 40)
+    let buttonImageViewOffSet = CGFloat(integerLiteral: 20)
+    let standardEdgeSpacing = CGFloat(integerLiteral: 20)
+    let imageViewHeight = CGFloat(integerLiteral: 200)
+    let infoStackEdgeSpacing = CGFloat(integerLiteral: 40)
+    let iconSideLength = CGFloat(integerLiteral: 20)
+    let infoStackIconLabelSpacing = CGFloat(integerLiteral: 15)
+    let avatarSpacing = CGFloat(integerLiteral: 5)
+    let infoTableSpacing = CGFloat(integerLiteral: 5)
+    
+    
     
     var event:Event?
     
@@ -45,36 +57,48 @@ class EventDetailViewController: UIViewController {
             make.width.equalTo(view.frame.width)
         }
         
+        let buttonStackBackground = UIView()
+        buttonStackBackground.backgroundColor = UIColor.darkSkyBlue
+        buttonStackBackground.layer.cornerRadius = buttonHeight / 2
+        buttonStackBackground.translatesAutoresizingMaskIntoConstraints = false
         //interested and going buttons
-        interestedButton.backgroundColor = UIColor.darkSkyBlue
-        goingButton.backgroundColor = UIColor.darkSkyBlue
+        interestedButton.snp.makeConstraints{ (make) -> Void in
+            make.height.equalTo(buttonHeight)
+        }
+        goingButton.snp.makeConstraints{ (make) -> Void in
+            make.height.equalTo(buttonHeight)
+        }
         let buttonStack = UIStackView(arrangedSubviews: [interestedButton, goingButton])
         buttonStack.alignment = .center
         buttonStack.axis = .horizontal
         buttonStack.distribution = .fillEqually
+        buttonStack.insertSubview(buttonStackBackground, at: 0)
+        buttonStackBackground.snp.makeConstraints{ (make) -> Void in
+            make.edges.equalTo(buttonStack)
+        }
         
         //table of info
-        let calendarIcon = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let calendarIcon = UIImageView(frame: CGRect(x: 0, y: 0, width: iconSideLength, height: iconSideLength))
         calendarIcon.image = UIImage(named: "magnifyingGlass")
         let calendarStack = UIStackView(arrangedSubviews: [calendarIcon, eventTime])
         calendarStack.alignment = .center
         calendarStack.axis = .horizontal
         calendarStack.distribution = .fill
-        calendarStack.spacing = 15
+        calendarStack.spacing = infoStackIconLabelSpacing
         
-        let participantIcon = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let participantIcon = UIImageView(frame: CGRect(x: 0, y: 0, width: iconSideLength, height: iconSideLength))
         participantIcon.image = UIImage(named: "magnifyingGlass")
         let participantImageAndStringStack = UIStackView(arrangedSubviews: [eventParticipants])
         var avatars:[UIImageView] = []
         for _ in event?.avatars ?? []{
             if avatars.count < 3 {  //support maximum 3 avatars, if more than 3, only add first three to the array
-                avatars.append(UIImageView(frame:CGRect(x: 0, y: 0, width: 20, height: 20)))
+                avatars.append(UIImageView(frame:CGRect(x: 0, y: 0, width: iconSideLength, height: iconSideLength)))
             }
         }
         for index in 0..<avatars.count {
             avatars[index].snp.makeConstraints{ (make) -> Void in
-                make.width.equalTo(20.0)
-                make.height.equalTo(20.0)
+                make.width.equalTo(iconSideLength)
+                make.height.equalTo(iconSideLength)
             }
             avatars[index].layer.cornerRadius = avatars[index].frame.height/2
             avatars[index].clipsToBounds = true
@@ -86,35 +110,35 @@ class EventDetailViewController: UIViewController {
         participantImageAndStringStack.axis = .horizontal
         participantImageAndStringStack.distribution = .fill
         participantImageAndStringStack.alignment = .center
-        participantImageAndStringStack.spacing = 5.0
+        participantImageAndStringStack.spacing = avatarSpacing
         let participantStack = UIStackView(arrangedSubviews: [participantIcon, participantImageAndStringStack])
         participantStack.alignment = .center
         participantStack.axis = .horizontal
         participantStack.distribution = .fill
-        participantStack.spacing = 15
+        participantStack.spacing = infoStackIconLabelSpacing
         
-        let organizerIcon = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let organizerIcon = UIImageView(frame: CGRect(x: 0, y: 0, width: iconSideLength, height: iconSideLength))
         organizerIcon.image = UIImage(named: "magnifyingGlass")
         let organizerStack = UIStackView(arrangedSubviews: [organizerIcon, eventOrganizer])
         organizerStack.alignment = .center
         organizerStack.axis = .horizontal
         organizerStack.distribution = .fill
-        organizerStack.spacing = 15
+        organizerStack.spacing = infoStackIconLabelSpacing
         
         
-        let locationIcon = UIImageView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
+        let locationIcon = UIImageView(frame: CGRect(x: 0, y: 0, width: iconSideLength, height: iconSideLength))
         locationIcon.image = UIImage(named: "magnifyingGlass")
         let locationStack = UIStackView(arrangedSubviews: [locationIcon, eventLocation])
         locationStack.alignment = .center
         locationStack.axis = .horizontal
         locationStack.distribution = .fill
-        locationStack.spacing = 15
+        locationStack.spacing = infoStackIconLabelSpacing
         
         let infoTableStack = UIStackView(arrangedSubviews: [calendarStack, participantStack, organizerStack, locationStack])
         infoTableStack.alignment = .leading
         infoTableStack.axis = .vertical
         infoTableStack.distribution = .fill
-        infoTableStack.spacing = 5
+        infoTableStack.spacing = infoTableSpacing
         
         
         //Add three dividers between the elements of infoTableStack
@@ -141,27 +165,27 @@ class EventDetailViewController: UIViewController {
             make.top.equalTo(contentView)
             make.left.equalTo(contentView)
             make.right.equalTo(contentView)
-            make.height.equalTo(200)
+            make.height.equalTo(imageViewHeight)
         }
         
         buttonStack.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(eventImage.snp.bottom)
-            make.left.equalTo(contentView).offset(20)
-            make.right.equalTo(contentView).offset(-20)
+            make.top.equalTo(eventImage.snp.bottom).offset(-buttonImageViewOffSet)
+            make.left.equalTo(contentView).offset(standardEdgeSpacing)
+            make.right.equalTo(contentView).offset(-standardEdgeSpacing)
         }
         
         eventDiscription.numberOfLines = 0
         eventDiscription.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(buttonStack.snp.bottom).offset(20)
-            make.left.equalTo(contentView).offset(20)
-            make.right.equalTo(contentView).offset(-20)
+            make.top.equalTo(buttonStack.snp.bottom).offset(standardEdgeSpacing)
+            make.left.equalTo(contentView).offset(standardEdgeSpacing)
+            make.right.equalTo(contentView).offset(-standardEdgeSpacing)
         }
         
         infoTableStack.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(eventDiscription.snp.bottom).offset(20)
-            make.left.equalTo(contentView).offset(20)
-            make.right.equalTo(contentView).offset(-20)
-            make.bottom.equalTo(contentView).offset(-20)
+            make.top.equalTo(eventDiscription.snp.bottom).offset(standardEdgeSpacing)
+            make.left.equalTo(contentView).offset(infoStackEdgeSpacing)
+            make.right.equalTo(contentView).offset(-infoStackEdgeSpacing)
+            make.bottom.equalTo(contentView).offset(-standardEdgeSpacing)
         }
         
 }
