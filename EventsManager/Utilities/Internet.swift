4d721d4ba2abf02ec4acfeee200ff0b569bd1768
Rodrigo Taipe <rt363@cornell.edu>
2019-04-14 02:39:06 -0400

    Add json parser helpers and modularize endpoints

--- EventsManager/Utilities/Internet.swift
+++ EventsManager/Utilities/Internet.swift
@@ -11,21 +11,11 @@ import Alamofire
 import SwiftyJSON
 
 class Internet {
-    #if DEVELOPMENT
-    private static let baseURL = "http://127.0.0.1:8000/"
-    #else
-    private static let baseURL = "http://cuevents-app.herokuapp.com/"
-    #endif
-    private static let serverTokenAddress = baseURL + "generate_token/"
-    private static let mediaAddress = baseURL + "app/media/"
-    private static let tagAddress = baseURL + "app/tag/"
-    private static let locationAddress = baseURL + "app/loc/"
-    private static let eventsFeedAddress = baseURL + "feed/events/"
-    private static let eventDetailsAddress = baseURL + "event/"
-    
 
     static func getServerAuthToken(for googleToken: String, _ completion: @escaping (String?) -> Void) {
-        Alamofire.request("\(serverTokenAddress)\(googleToken)/").validate().responseJSON { response in
+        let qp = [Endpoint.QueryParam.googleToken : googleToken]
+        let URL = Endpoint.getURLString(address: .serverTokenAddress, queryParams: qp)
+        Alamofire.request(URL).validate().responseJSON { response in
             switch response.result {
                 case .success(let value):
                     let json = JSON(value)
@@ -37,25 +27,14 @@ class Internet {
         }
     }
     
-    static func fetchMedia(serverToken:String, mediaPk:Int, completion: @escaping (URL?) -> Void) {
-        var headers = Alamofire.SessionManager.defaultHTTPHeaders
-        headers["Authorization"] = serverToken
-        Alamofire.request("\(mediaAddress)\(mediaPk)", headers: headers).validate().responseString { response in
-            switch response.result {
-            case .success(let value):
-                let imageURL = URL(string: value)
-                completion(imageURL)
-            case .failure(let error):
-                print(error)
-                completion(nil)
-            }
-        }
-    }
-    
     static func fetchTag(serverToken: String, tagPk:Int , completion: @escaping (Tag?) -> Void) {
         var headers = Alamofire.SessionManager.defaultHTTPHeaders
         headers["Authorization"] = serverToken
-        Alamofire.request("\(tagAddress)\(tagPk)", headers: headers).validate().responseJSON { response in
+        
+        let qp = [Endpoint.QueryParam.tagPk : String(tagPk)]
+        let URL = Endpoint.getURLString(address: .tagAddress, queryParams: qp)
+        
+        Alamofire.request(URL, headers: headers).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
@@ -77,7 +56,11 @@ class Internet {
     static func fetchLocation(serverToken: String, locationPk:Int , completion: @escaping (String?, String?) -> Void) {
         var headers = Alamofire.SessionManager.defaultHTTPHeaders
         headers["Authorization"] = serverToken
-        Alamofire.request("\(locationAddress)\(locationPk)", headers: headers).validate().responseJSON { response in
+        
+        let qp = [Endpoint.QueryParam.locationPk: String(locationPk)]
+        let URL = Endpoint.getURLString(address: .locationAddress, queryParams: qp)
+        
+        Alamofire.request(URL, headers: headers).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
@@ -108,7 +91,9 @@ class Internet {
             "end": DateFormatHelper.datetime(from: end)
         ]
         
-        Alamofire.request(eventsFeedAddress, parameters: params, headers: headers).validate().responseJSON
+        let URL = Endpoint.getURLString(address: .eventsFeedAddress, queryParams: [:])
+        
+        Alamofire.request(URL, parameters: params, headers: headers).validate().responseJSON
         { response in
             switch response.result {
             case .success(let value):
@@ -117,33 +102,9 @@ class Internet {
                 var events : [Event] = []
                 if let updatedJSON = json["updated"].array {
                     for subJSON in updatedJSON {
-                        if let pk = subJSON["pk"].int,
-                        let name = subJSON["name"].string,
-                        let description = subJSON["description"].string,
-                        let start_date = subJSON["start_date"].string,
-                        let end_date = subJSON["end_date"].string,
-                        let start_time = subJSON["start_time"].string,
-                        let end_time = subJSON["end_time"].string,
-                        let num_attendees = subJSON["num_attendees"].int,
-                        let is_public = subJSON["is_public"].bool,
-                        let organizer = subJSON["organizer"].int,
-                        let location = subJSON["location"].int,
-                        let event_tags = subJSON["event_tags"].array,
-                        let event_media = subJSON["event_media"].array
-                        {
-                            let startDateTime = DateFormatHelper.datetime(from: "\(start_date) \(start_time)")
-                            let endDateTime = DateFormatHelper.datetime(from: "\(end_date) \(end_time)")
-                            var imageURL = URL(string: "http://ethanhu.me/images/background.jpg")!
-                            if event_media.count > 0 {
-                                let imageId = event_media[0].int!
-                                imageURL = URL(string: mediaAddress + String(imageId))!
-                            }
-                            var eventTags : [Int] = []
-                            for tag in event_tags {
-                                eventTags.append((tag.int)!)
-                            }
-                            let eventInstance = Event.init(id: pk, startTime: startDateTime!, endTime: endDateTime!, eventName: name, eventLocation: location, eventImage: imageURL, eventOrganizer: organizer, eventDescription: description, eventTags: eventTags, eventParticipantCount: num_attendees, isPublic: is_public)
-                            events.append(eventInstance)
+                        let event = JSONParserHelper.parseEvent(json: subJSON)
+                        if event != nil {
+                            events.append(event!)
                         }
                     }
                 }
@@ -174,50 +135,43 @@ class Internet {
     static func fetchEventDetails(serverToken: String, id: Int, completion: @escaping (Event?) -> Void) {
         print(serverToken)
         let headers : HTTPHeaders = ["Authorization" : serverToken]
-        Alamofire.request("\(eventDetailsAddress)\(id)/", headers: headers).validate().responseJSON
+        
+        let qp = [Endpoint.QueryParam.eventPk : String(id)]
+        let URL = Endpoint.getURLString(address: .eventDetailsAddress, queryParams: qp)
+        
+        Alamofire.request(URL, headers: headers).validate().responseJSON
             { response in
                 switch response.result {
                 case .success(let value):
                     let json = JSON(value)
                     print("SUCCESS: \(json)")
-                    if let pk = json["pk"].int,
-                        let name = json["name"].string,
-                        let description = json["description"].string,
-                        let start_date = json["start_date"].string,
-                        let end_date = json["end_date"].string,
-                        let start_time = json["start_time"].string,
-                        let end_time = json["end_time"].string,
-                        let num_attendees = json["num_attendees"].int,
-                        let is_public = json["is_public"].bool,
-                        let organizer = json["organizer"].int,
-                        let location = json["location"].int,
-                        let event_tags = json["event_tags"].array,
-                        let event_media = json["event_media"].array
-                    {
-                        let startDateTime = DateFormatHelper.datetime(from: "\(start_date) \(start_time)")
-                        let endDateTime = DateFormatHelper.datetime(from: "\(end_date) \(end_time)")
-                        var imageURL = URL(string: "http://ethanhu.me/images/background.jpg")!
-                        if event_media.count > 0 {
-                            let imageId = event_media[0].int!
-                            imageURL = URL(string: mediaAddress + String(imageId))!
-                        }
-                        var eventTags : [Int] = []
-                        for tag in event_tags {
-                            eventTags.append((tag.int)!)
-                        }
-                        let eventInstance = Event.init(id: pk, startTime: startDateTime!, endTime: endDateTime!, eventName: name, eventLocation: location, eventImage: imageURL, eventOrganizer: organizer, eventDescription: description, eventTags: eventTags, eventParticipantCount: num_attendees, isPublic: is_public)
-                        completion(eventInstance)
-                    }
-                    else {
-                        completion(nil)
-                    }
-                    
+                    completion(JSONParserHelper.parseEvent(json: json))
                 case .failure(let error):
                     print("ERROR: \(error)")
                     completion(nil)
                 }
         }
         
+    }
+    
+    static func fetchSingleTag(serverToken: String, id: Int, completion: @escaping (Tag?) -> Void){
+        let headers : HTTPHeaders = ["Authorization" : serverToken]
+        
+        let qp = [Endpoint.QueryParam.tagPk : String(id)]
+        let URL = Endpoint.getURLString(address: .tagAddress, queryParams: qp)
+        
+        Alamofire.request(URL, headers: headers).validate().responseJSON
+            { response in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    print("SUCCESS: \(json)")
+                    completion(JSONParserHelper.parseTag(json: json))
+                case .failure(let error):
+                    print("ERROR: \(error)")
+                    completion(nil)
+                }
+            }
     }
 
 }
